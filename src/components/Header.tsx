f07ff3c431260e62bdac4ec1b6d283328954d259
Luka <xaraishvili.luka@list.ru>
2026-01-05 13:59:37 +0400

    initial commit;

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from './assets/Logo.png';
+
+const Header = () => {
+  return (
+    <>
+    <img src='./assets/Logo.png' alt="logo" />
+    
+    </>
+
+  )
+}
+
+export default Header

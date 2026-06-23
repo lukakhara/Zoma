e46e6443a6caf38ae29cf101e2786852d8b1c28d
Luka <xaraishvili.luka@list.ru>
2026-06-23 10:26:37 +0400

    Updates

--- assets/NotFound-CaP2jNt6.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-CjWYZHGP.js";const r=()=>t.jsx(t.Fragment,{children:t.jsx("div",{children:"404 Page is NotFound "})});export{r as default};
---
+++ assets/NotFound-DScMc-HT.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-OnkagtE3.js";const r=()=>t.jsx(t.Fragment,{children:t.jsx("div",{children:"404 Page is NotFound "})});export{r as default};
